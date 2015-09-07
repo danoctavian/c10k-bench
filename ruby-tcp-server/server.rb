74eac84989c465efe2792f4ac521e3f621a1ae32
dan <danoctavian91@gmail.com>
2015-09-07 01:06:37 -0700

    added ruby event machine.

---
+++ ruby-tcp-server/server.rb
@@ -0,0 +1,19 @@
+require 'eventmachine'
+
+module EchoServer
+ def post_init
+ end
+
+ def receive_data data
+   send_data data
+   close_connection if data =~ /quit/i
+ end
+
+ def unbind
+ end
+end
+
+# Note that this will block current thread.
+EventMachine.run {
+  EventMachine.start_server "127.0.0.1", 4000, EchoServer
+}
